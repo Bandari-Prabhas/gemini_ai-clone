749e8eecb7ac302450d935c95805064a9a1804e3
Bandari-Prabhas <157564723+Bandari-Prabhas@users.noreply.github.com>
2024-08-07 17:54:51 +0530

    Add files via upload

---
+++ components/App.jsx
@@ -0,0 +1,16 @@
+// 
+import React from 'react'
+import Sidebar from './Sidebar/Sidebar'
+import Main from './Main/Main'
+
+function App() {
+  return (
+    <>
+      
+      <Sidebar />
+      <Main />
+    </>
+  )
+}
+
+export default App
